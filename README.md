5d06f4c9d1581e5d263e232ccd3359ea9e5bdf1f
Vincent Royer <vroyer@strapdata.com>
2018-10-17 21:42:52 +0200

    fixup 3

--- README.md
+++ README.md
@@ -1,21 +1,15 @@
 # docker-elassandra-enterprise
 
-Build elassandra-enterprise docker image
+![Elassandra Logo](elassandra-logo.png)
 
-## build
+Build **elassandra-enterprise** docker image. 
 
-	ENTERPRISE_PLUGIN_DIR=/Users/vroyer/dev/git/strapdata/strapack BASE_IMAGE=strapdata/elassandra-rc:6.2.3.7-rc1 DOCKER_PUBLISH=true ./build.sh 
+**Elassandra Enterprise** is a commercial plugin providing additional features for [Elassandra](https://github.com/strapdata/elassandra), elasticsearch monitoring, security and the ability to execute elasticsearch query from your favorite CQL driver. Check-out the [elassandra enterprise documentation](http://doc.elassandra.io/en/latest/enterprise.html) for detailed instructions.
 
+Commercial support is available from [Strapdata](https://www.strapdata.com).
 
-## Visit elassandra image 
+## Build
 
-Run bash in image:
-
-	docker run --rm -it --entrypoint /bin/bash strapdata/elassandra-enterprise:6.2.3.7-rc1
-
-## run elassandra
-
-Lauch an Elassandra node:
-	
-	docker run -d strapdata/elassandra-enterprise:6.2.3.7-rc1
-	docker exec -it <container_id> bash
+	ENTERPRISE_PLUGIN_URL=https://packages.strapdata.com/strapdata-plugin-6.latest.zip \
+	BASE_IMAGE=strapdata/elassandra:6.2.3.x \
+	./build.sh
--- ready-probe.sh
+++ ready-probe.sh
@@ -5,14 +5,15 @@
 # MONITOR_SSL_CLIENT_CERT could be your client-auth certificate and keystore (filename of a mounted k8s secret)
 # MONITOR_SSL_CLIENT_CERT_TYPE should be your client-auth certificate file type, p12 by default
 #set -x
+POD_IP=${POD_IP:-$(hostname --ip-address)}
 
-if [[ $(nodetool status | grep ${POD_IP:-localhost}) == *"UN"* ]]; then
+if [[ $(nodetool status | grep ${POD_IP}) == *"UN"* ]]; then
   if [[ "${CASSANDRA_DAEMON:-org.apache.cassandra.service.CassandraDaemon}" == "org.apache.cassandra.service.CassandraDaemon" ]]; then
      exit 0;
   else 
-     rc=$(curl -sLI -k -w '%{http_code}\n' ${MONITOR_SSL_CLIENT_CERT:+"--cert"} ${MONITOR_SSL_CLIENT_CERT} ${MONITOR_SSL_CLIENT_CERT:+"--cert-type"} ${MONITOR_SSL_CLIENT_CERT:+"${MONITOR_SSL_CLIENT_CERT_TYPE:-p12}"} -o /dev/null --max-time ${READINESS_PROBE_TIMEOUT:-30} "${PROTOCOL:-http}://${POD_IP:-localhost}:9200/")
+     rc=$(curl -sLI -k -w '%{http_code}\n' ${MONITOR_SSL_CLIENT_CERT:+"--cert"} ${MONITOR_SSL_CLIENT_CERT} ${MONITOR_SSL_CLIENT_CERT:+"--cert-type"} ${MONITOR_SSL_CLIENT_CERT:+"${MONITOR_SSL_CLIENT_CERT_TYPE:-p12}"} -o /dev/null --max-time ${READINESS_PROBE_TIMEOUT:-30} "${PROTOCOL:-http}://${POD_IP}:9200/")
      if [[ $DEBUG ]]; then
-        echo "Elasticsearch rc="+rc;
+        echo "Elasticsearch check rc=${rc}";
      fi
      case "$rc" in
      "200") exit 0;;
